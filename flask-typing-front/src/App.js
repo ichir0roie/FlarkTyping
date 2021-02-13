028438888a7a28e14a4f09a74c6748762822e1e3
IchiroT <ichir0roie@gmail.com>
2021-02-13 14:18:34 +0900

    project adjusted

--- flask-typing-front/src/App.js
+++ flask-typing-front/src/App.js
@@ -1,36 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState } from 'react';
+import "./App.css";
+import React, { useState } from "react";
 
-var nowQuestion="test text";
-
+var nowQuestion = "test text";
 
 function App() {
+	const handleOnChange = (e) => {
+		nowQuestion = e.target.value;
+		chgTex(nowQuestion);
+	};
 
-  const handleOnChange=(e)=>{
-    nowQuestion=e.target.value;
-    chgTex(nowQuestion);
-  }
-
-  
-
-  const [inptTex, chgTex] =useState("");
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p className="App-title">FlaskTyping</p>
-        <p>this is header.</p>
-      </header>
-      <body className="App-body">
-        <p>this is body</p>
-        <p className="App-question">{nowQuestion}</p>
-        <input className="App-ask"
-        onChange={(e)=>handleOnChange(e)}
-        ></input>
-        <p>{inptTex}</p>
-      </body>
-    </div>
-  );
+	const [inptTex, chgTex] = useState("");
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p className="App-title">FlaskTyping</p>
+				<p>this is header.</p>
+			</header>
+			<body className="App-body">
+				<p>this is body</p>
+				<p className="App-question">{nowQuestion}</p>
+				<input className="App-ask" onChange={(e) => handleOnChange(e)}></input>
+				<p>{inptTex}</p>
+			</body>
+			a
+		</div>
+	);
 }
 
 export default App;
