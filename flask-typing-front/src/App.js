0e787b5f9a7e3a63b3a8afd26daa20e1513e9d05
IchiroT <ichir0roie@gmail.com>
2021-02-13 12:12:53 +0900

    Revert "del folder"
    This reverts commit f59582b3f41147eca28f82b56310663ee290f8be.

---
+++ flask-typing-front/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+
+var nowQuestion="test text";
+
+
+function App() {
+
+  const handleOnChange=(e)=>{
+    nowQuestion=e.target.value;
+    chgTex(nowQuestion);
+  }
+
+  
+
+  const [inptTex, chgTex] =useState("");
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="App-title">FlaskTyping</p>
+        <p>this is header.</p>
+      </header>
+      <body className="App-body">
+        <p>this is body</p>
+        <p className="App-question">{nowQuestion}</p>
+        <input className="App-ask"
+        onChange={(e)=>handleOnChange(e)}
+        ></input>
+        <p>{inptTex}</p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
